2c3f2d4ffb4dc1da3ccb0e11215b4f42aa12837d
Mark Lovitt McCormick <mark.mccormick@aciworldwide.com>
2014-12-01 04:32:00 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,8 @@ if [ "$1" = 'couchdb' ]; then
   chmod 664 /usr/local/etc/couchdb/*.ini
   chmod 775 /usr/local/etc/couchdb/*.d
   HOME=/var/lib/couchdb exec gosu couchdb "$@"
+  
+  curl -X PUT http://192.168.59.103:5984/baseball
 fi
 
 exec "$@"
